04ec9977aab4c06ed25d9d51c14f7975a0dc2b00
shegai01 <126394697+shegai01@users.noreply.github.com>
2025-05-05 22:54:34 +0900

    Time: 0 ms (100%), Space: 6.1 MB (11.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    hash := make(map[int]int)
+    // target :=0
+    for i, val := range nums{
+        res := target - val
+        if ind, found := hash[res]; found{
+            return []int{ind, i}
+        }
+        hash[val]= i
+    }
+    return []int{}
+}
