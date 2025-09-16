5be68b596f15e2bd3af05c78b938d6e331243e83
shegai01 <126394697+shegai01@users.noreply.github.com>
2025-09-16 11:44:29 +0300

    Time: 0 ms (100%), Space: 6.3 MB (62.2%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
@@ -0,0 +1,11 @@
+func removeDuplicates(nums []int) int {
+    pivot := 1
+    for i :=1; i < len(nums); i++{
+        if nums[i] != nums[i-1]{
+            nums[pivot] = nums[i]
+            pivot++
+        }
+    }
+    return pivot
+    
+}
