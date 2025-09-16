448c877ecf7c3d87ced1ad2cdbadbb4771c9e563
shegai01 <126394697+shegai01@users.noreply.github.com>
2025-09-16 12:17:37 +0300

    Time: 0 ms (100%), Space: 4 MB (99.43%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+    if len(nums) == 0{
+        return 0
+    }
+    count := 0
+    for i := 0; i < len(nums); i++{
+        if nums[i] != val{
+            nums[count] = nums[i]
+            count++
+        }
+    }
+    return count
+}
